0680f25217f8570294b2bb56cf511466e9fca0a2
margooowa <margooowa@ukr.net>
2014-07-17 11:19:19 +0300

    proba

--- ClientBank/src/view/AdminWindow.java
+++ ClientBank/src/view/AdminWindow.java
@@ -14,6 +14,7 @@ import java.awt.event.MouseEvent;
 import java.rmi.RemoteException;
 import java.util.HashMap;
 import java.util.List;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
